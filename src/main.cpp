92a221a3c92919e8daf7fefbd708765632bf0baa
H Fuc <hoangphuc2k64@gmail.com>
2026-08-03 10:04:10 +0700

    3/8/2026

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ArduinoJson.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-// !!! Thay <IP_LAN_CUA_MAY_BAN> bang IP that cua may tinh ban (vi du 192.168.1.5) !!!
 const char* proxyURL = "http://192.168.0.105:3000/";
 
 #define SS_PIN   5
@@ -16,9 +18,31 @@ const char* proxyURL = "http://192.168.0.105:3000/";
 #define MISO_PIN 13
 #define MOSI_PIN 11
 #define BUZZER_PIN 17
+#define RELAY_PIN 16
+
+#define OLED_SDA 8
+#define OLED_SCL 9
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
 
 MFRC522 rfid(SS_PIN, RST_PIN);
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
+
 unsigned long lastScan = 0;
+
+void hienThi(String dong1, String dong2 = "") {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 10);
+  display.println(dong1);
+  if (dong2 != "") {
+    display.setTextSize(1);
+    display.setCursor(0, 40);
+    display.println(dong2);
+  }
+  display.display();
+}
 
 bool kiemTraSinhVien(String uid, String &hoTenOut, String &lopOut) {
   if (WiFi.status() != WL_CONNECTED) {
@@ -32,13 +56,13 @@ bool kiemTraSinhVien(String uid, String &hoTenOut, String &lopOut) {
   Serial.println(url);
 
   http.begin(url);
-  http.setTimeout(20000); // tang timeout vi Apps Script co the mat vai giay de xu ly + tinh thong ke
+  http.setTimeout(20000);
   int httpCode = http.GET();
 
   if (httpCode != 200) {
     Serial.print("Loi HTTP, ma: ");
     Serial.println(httpCode);
-    hoTenOut = "Loi ket noi toi server proxy";
+    hoTenOut = "Loi ket noi";
     http.end();
     return false;
   }
@@ -53,7 +77,7 @@ bool kiemTraSinhVien(String uid, String &hoTenOut, String &lopOut) {
   if (err) {
     Serial.print("Loi parse JSON: ");
     Serial.println(err.c_str());
-    hoTenOut = "Loi du lieu tra ve";
+    hoTenOut = "Loi du lieu";
     return false;
   }
 
@@ -64,18 +88,35 @@ bool kiemTraSinhVien(String uid, String &hoTenOut, String &lopOut) {
     return true;
   }
 
-  hoTenOut = "Khong xac dinh (UID la, chua dang ky)";
+  hoTenOut = "The la";
   return false;
 }
 
+void moCua() {
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(2000);
+  digitalWrite(RELAY_PIN, LOW);
+}
+
+void bipThuCong(int tanSo, int thoiGianMs) {
+  int chuKy = 1000000 / tanSo; // micro giay cho 1 chu ky
+  long soChuKy = (long)thoiGianMs * 1000 / chuKy;
+  for (long i = 0; i < soChuKy; i++) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delayMicroseconds(chuKy / 2);
+    digitalWrite(BUZZER_PIN, LOW);
+    delayMicroseconds(chuKy / 2);
+  }
+}
+
 void baoThanhCong() {
-  tone(BUZZER_PIN, 2000, 150);
+  bipThuCong(2000, 150);
 }
 
 void baoThatBai() {
-  tone(BUZZER_PIN, 400, 120);
+  bipThuCong(400, 120);
   delay(180);
-  tone(BUZZER_PIN, 400, 120);
+  bipThuCong(400, 120);
 }
 
 void setup() {
@@ -85,8 +126,19 @@ void setup() {
   SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);
   rfid.PCD_Init();
 
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("Khong tim thay OLED");
+  }
+  display.clearDisplay();
+  display.display();
+
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+
   Serial.println();
   Serial.println("RFID Ready");
+  hienThi("READY", "Quet the de vao");
 
   WiFi.begin(ssid, password);
   Serial.print("Dang ket noi WiFi");
@@ -101,7 +153,8 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   delay(1000);
-  Serial.println("San sang. Quet the RFID de diem danh.");
+  Serial.println("San sang. Quet the RFID de vao cua.");
+  hienThi("READY", "Quet the de vao");
 }
 
 void loop() {
@@ -125,16 +178,24 @@ void loop() {
   Serial.print("UID : ");
   Serial.println(uid);
 
+  hienThi("Dang kiem tra...");
+
   String hoTen = "", lop = "";
   bool ok = kiemTraSinhVien(uid, hoTen, lop);
 
   if (ok) {
-    Serial.println("Hop le - " + hoTen + " (Lop: " + lop + ")");
+    Serial.println("Hop le - " + hoTen + " (Lop: " + lop + ") - MO CUA");
+    hienThi("Moi vao", hoTen);
     baoThanhCong();
+    moCua();
   } else {
-    Serial.println(hoTen);
+    Serial.println("Khong hop le: " + hoTen);
+    hienThi("THE KHONG", "DUNG");
     baoThatBai();
   }
+
+  delay(1000);
+  hienThi("READY", "Quet the de vao");
 
   rfid.PICC_HaltA();
 }
